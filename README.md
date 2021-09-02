ee45e1cfc9608add26e445fbf9b58b8b74873c88
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2021-09-02 13:54:22 +0200

    Improve command line compatibility

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # The Lang Programming Language
+
 This project makes simple translation files Turing complete
 
 ### "TermIO-Control" window commands
+
 **Execution of lang file**: Type "executeLang -*Path to .lang file*" in the white text input in the "TermIO-Control" window<br>
-You can also run "java -jar LangCompiler.jar path/to/file.lang" in a terminal or console.<br>
 **LangShell**: Type "startShell" in the white text input in the "TermIO-Control" window<br>
-You can also run "java -jar LangCompiler.jar -startShell" in a terminal or console.<br>
 **Language definitions**: See comment in src/me/jddev0/module/io/Lang.java and checkout assets/test.lang (It contains a basic overview of the programming language with explanations)<br>
 **Language Tutorial**: You can find many tutorial lang files in /assets/tuts/.<br>
 **Print AST tree**: Type "printAST -*Path to .lang file*" for parsing a Lang file and printing the parsed AST tree<br>
 **4K-Support**: Type "toggle4k" for a larger font in the "TermIO-Control" window and the "LangShell" window<br>
 
+#### "TermIO-Control" window commands from terminal/console
+
+You can also run "java -jar LangCompiler.jar -**command** **args**" in a terminal or console.<br>
+
 ### Breaking changes
+
 - **v1.0.0**:
   - Arrays variable names starting with "&LANG_" are no longer allowed
   - Var/Array pointer names
   - Many deprecated methods and classes in the Lang class won't work as expected
+
--- src/me/jddev0/startup/Startup.java
+++ src/me/jddev0/startup/Startup.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 import me.jddev0.module.graphics.LangShellWindow;
@@ -11,18 +12,48 @@ import me.jddev0.module.graphics.TerminalWindow;
 import me.jddev0.module.io.Lang;
 import me.jddev0.module.io.LangParser;
 import me.jddev0.module.io.LangPlatformAPI;
+import me.jddev0.module.io.ReaderActionObject;
 import me.jddev0.module.io.TerminalIO;
 import me.jddev0.module.io.TerminalIO.Level;
 
 public class Startup {
 	private static boolean is4k = false;
-	
-	private static int getFontSize() {
-		return is4k?24:12;
-	}
+	private static LangPlatformAPI langPlatformAPI = new LangPlatformAPI();
 	
 	public static void main(String[] args) {
-		LangPlatformAPI langPlatformAPI = new LangPlatformAPI();
+		if(args.length > 0 && (!args[0].startsWith("-") || args[0].startsWith("--") || args[0].startsWith("-h"))) {
+			if(args[0].startsWith("-h")) {
+				printHelp();
+				return;
+			}
+			
+			if(args[0].startsWith("--")) {
+				if(!args[0].equals("--help"))
+					System.err.printf("Unknown COMMAND \"%s\"\n", args[0]);
+				
+				printHelp();
+				return;
+			}
+			
+			String langFile = args[0];
+			boolean printTranslations = false;
+			
+			for(int i = 1;i < args.length;i++) {
+				String arg = args[i];
+				if(arg.equals("-printTranslations")) {
+					printTranslations = true;
+				}else {
+					System.err.printf("Unknown EXECUTION_ARG \"%s\"\n", arg);
+					
+					printHelp();
+					return;
+				}
+			}
+			
+			executeLangFile(langFile, printTranslations);
+			return;
+		}
+		
 		TerminalWindow termWin = new TerminalWindow(getFontSize());
 		TerminalIO term = new TerminalIO(new File("assets/log.txt"));
 		term.addCommand("executeLang", input -> {
@@ -87,6 +118,8 @@ public class Startup {
 			
 			is4k = !is4k;
 			termWin.setFontSize(getFontSize());
+		}).addCommand("printHelp", input -> {
+			printHelp();
 		}).addCommand("exit", input -> {
 			if(input.length != 0) {
 				term.logf(Level.ERROR, "To many arguments: %d/0!\n", Startup.class, input.length);
@@ -114,13 +147,72 @@ public class Startup {
 		termWin.setTerminalIO(term);
 		termWin.setVisible(true);
 		
-		if(args.length == 1) {
-			if(args[0].equals("-startShell")) {
-				LangShellWindow langShellWin = new LangShellWindow(termWin, term, getFontSize());
-				langShellWin.setVisible(true);
-			}else {
-				term.getCommands().get("executeLang").action(args);
+		if(args.length > 0) {
+			if(args[0].length() < 2) {
+				System.err.printf("Unknown COMMAND \"%s\"\n", args[0]);
+				
+				printHelp();
+				
+				System.exit(0);
+				return;
 			}
+			String command = args[0].substring(1);
+			
+			String[] commandArgs = Arrays.copyOfRange(args, 1, args.length);
+			ReaderActionObject commandFunction = term.getCommands().get(command);
+			if(commandFunction == null) {
+				System.err.printf("Unknown COMMAND \"%s\"\n", args[0]);
+				
+				printHelp();
+				
+				System.exit(0);
+				return;
+			}
+			commandFunction.action(commandArgs);
+		}
+	}
+	
+	private static int getFontSize() {
+		return is4k?24:12;
+	}
+	
+	private static void printHelp() {
+		System.out.println("Usage: lang COMMAND [ARGs]... | lang FILE [EXECUTION_ARGs]...");
+		System.out.println("Interprets a Lang file");
+		System.out.println();
+		System.out.println("COMMANDs");
+		System.out.println("--------");
+		System.out.println("    -executeLang -FILE      Executes a Lang file in the \"TermIO-Control\" window");
+		System.out.println("    -printAST -FILE         Prints an AST of a Lang file to standard output");
+		System.out.println("    -startShell             Opens the \"LangShell\" window");
+		System.out.println("    -toogle4k               Changes the fontSize");
+		System.out.println("    -printHelp              Prints this help page");
+		System.out.println("    -exit                   Exits the \"TermIO-Control\" window");
+		System.out.println("    -commands               Lists all \"TermIO-Control\" window commands");
+		System.out.println("");
+		System.out.println("    -h, --help              Prints this help page");
+		System.out.println();
+		System.out.println("EXECUTION_ARGs");
+		System.out.println("--------------");
+		System.out.println("    -printTranslations      Prints all Translations after execution of Lang file finished to standard output");
+	}
+	
+	private static void executeLangFile(String langFile, boolean printTranslations) {
+		File lang = new File(langFile);
+		if(!lang.exists()) {
+			System.err.printf("The lang file %s wasn't found!\n", langFile);
+			
+			return;
+		}
+		
+		try {
+			Map<String, String> translations = Lang.getTranslationMap(langFile, true, null, langPlatformAPI);
+			if(printTranslations) {
+				System.out.println("-------------- Translations --------------");
+				translations.forEach((key, value) -> System.out.printf("%s = %s\n", key, value));
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
