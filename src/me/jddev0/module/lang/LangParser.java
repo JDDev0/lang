1e4d891be09bd5881af1cd505da9c57feb46e4e9
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-01-11 13:54:29 +0100

    Improve code for parsing function calls in operator parsing mode

--- src/me/jddev0/module/lang/LangParser.java
+++ src/me/jddev0/module/lang/LangParser.java
@@ -774,37 +774,8 @@ public final class LangParser {
 			}
 			
 			//Function calls
-			if(LangPatterns.matches(token, LangPatterns.PARSING_STARTS_WITH_FUNCTION_CALL)) {
-				if(whitespaces.length() > 0) {
-					builder.append(whitespaces.toString());
-					whitespaces.delete(0, whitespaces.length());
-				}
-				
-				if(builder.length() > 0) {
-					leftNodes.add(parseLRvalue(builder.toString(), null, true).convertToNode());
-					builder.delete(0, builder.length());
-				}
-				
-				int parameterStartIndex = token.indexOf('(');
-				int parameterEndIndex = LangUtils.getIndexOfMatchingBracket(token, parameterStartIndex, Integer.MAX_VALUE, '(', ')');
-				if(parameterEndIndex == -1) {
-					leftNodes.add(new AbstractSyntaxTree.ParsingErrorNode(ParsingError.BRACKET_MISMATCH, "Bracket in condition is missing"));
-					
-					break;
-				}
-				
-				String functionCall = token.substring(0, parameterEndIndex + 1);
-				token = token.substring(parameterEndIndex + 1);
-				
-				String functionName = functionCall.substring(0, parameterStartIndex);
-				String functionParameterList = functionCall.substring(parameterStartIndex + 1, functionCall.length() - 1);
-				
-				leftNodes.add(new AbstractSyntaxTree.FunctionCallNode(parseFunctionParameterList(functionParameterList, false).getChildren(), functionName));
-				continue;
-			}
-			
-			//Function calls without prefix
-			if(AbstractSyntaxTree.OperationNode.OperatorType.MATH.isCompatibleWith(type) && LangPatterns.matches(token, LangPatterns.PARSING_STARTS_WITH_FUNCTION_CALL_WITHOUT_PREFIX)) {
+			if(LangPatterns.matches(token, LangPatterns.PARSING_STARTS_WITH_FUNCTION_CALL) ||
+			(AbstractSyntaxTree.OperationNode.OperatorType.MATH.isCompatibleWith(type) && LangPatterns.matches(token, LangPatterns.PARSING_STARTS_WITH_FUNCTION_CALL_WITHOUT_PREFIX))) {
 				if(whitespaces.length() > 0) {
 					builder.append(whitespaces.toString());
 					whitespaces.delete(0, whitespaces.length());
