7aa01a69a5ebb158fd227de38b62ba5b08e4248e
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-01-23 18:33:55 +0100

    Add new assignments: "?=" (Set condition assignment) and ":=" (Set math assignment)

--- src/me/jddev0/module/lang/LangParser.java
+++ src/me/jddev0/module/lang/LangParser.java
@@ -967,6 +967,12 @@ public final class LangParser {
 					case "$":
 						operator = AbstractSyntaxTree.OperationNode.Operator.NON;
 						break;
+					case "?":
+						operator = AbstractSyntaxTree.OperationNode.Operator.CONDITIONAL_NON;
+						break;
+					case ":":
+						operator = AbstractSyntaxTree.OperationNode.Operator.MATH_NON;
+						break;
 					
 					default:
 						operator = null;
@@ -974,6 +980,10 @@ public final class LangParser {
 				
 				if(operator == null)
 					rvalueNode = new AbstractSyntaxTree.ParsingErrorNode(ParsingError.INVALID_ASSIGNMENT);
+				else if(operator == AbstractSyntaxTree.OperationNode.Operator.CONDITIONAL_NON)
+					rvalueNode = parseCondition(tokens[1]);
+				else if(operator == AbstractSyntaxTree.OperationNode.Operator.MATH_NON)
+					rvalueNode = parseMathExpr(tokens[1]);
 				else if(operator == AbstractSyntaxTree.OperationNode.Operator.NON)
 					rvalueNode = parseOperationExpr(tokens[1]);
 				else
