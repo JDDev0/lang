34563cace5a491ce2a6ddc0dc804cca94d0a9d82
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-04-23 16:40:21 +0200

    Add new combinator function: "func.combO2()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -3146,6 +3146,22 @@ final class LangPredefinedFunctions {
 				return interpreter.callFunctionPointer(bFunc, b.getVariableName(), argsB, INNER_SCOPE_ID);
 			}, SCOPE_ID);
 		});
+		funcs.put("combO2", (argumentList, SCOPE_ID) -> {
+			return combinatorFunctionHelper(argumentList, 3, new int[] {0, 1}, (Combinator3ArgFunction)(a, b, c, INNER_SCOPE_ID) -> {
+				FunctionPointerObject aFunc = a.getFunctionPointer();
+				FunctionPointerObject bFunc = b.getFunctionPointer();
+				
+				List<DataObject> argsB = new LinkedList<>();
+				List<DataObject> argsA = new LinkedList<>();
+				argsA.add(b);
+				argsA.add(new DataObject().setArgumentSeparator(", "));
+				argsA.add(c);
+				DataObject retA = interpreter.callFunctionPointer(aFunc, a.getVariableName(), argsA, INNER_SCOPE_ID);
+				argsB.add(retA == null?new DataObject().setVoid():retA);
+				
+				return interpreter.callFunctionPointer(bFunc, b.getVariableName(), argsB, INNER_SCOPE_ID);
+			}, SCOPE_ID);
+		});
 		funcs.put("combP", (argumentList, SCOPE_ID) -> {
 			return combinatorFunctionHelper(argumentList, 4, new int[] {0, 1, 2}, (Combinator4ArgFunction)(a, b, c, d, INNER_SCOPE_ID) -> {
 				FunctionPointerObject aFunc = a.getFunctionPointer();
