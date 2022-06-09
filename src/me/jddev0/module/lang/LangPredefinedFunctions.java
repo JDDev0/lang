e42996fddc021ab0a45d1cae9b4bbca85bd73b43
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-06-09 23:21:54 +0200

    Fix args in "func.arrayClear()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -4983,9 +4983,13 @@ final class LangPredefinedFunctions {
 		funcs.put("arrayClear", new LangPredefinedFunctionObject() {
 			@Override
 			public DataObject callFunc(List<DataObject> argumentList, final int SCOPE_ID) {
-				DataObject arrPointerObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, false);
-				if(argumentList.size() > 0) //Not 1 argument
+				List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+				if(combinedArgumentList.size() < 1)
+					return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+				if(combinedArgumentList.size() > 1)
 					return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+				
+				DataObject arrPointerObject = combinedArgumentList.get(0);
 				
 				if(arrPointerObject.getType() != DataType.ARRAY)
 					return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARR_PTR, SCOPE_ID);
