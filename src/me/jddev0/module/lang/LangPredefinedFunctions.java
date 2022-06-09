038893e2237bbbe453a90c0ea353e39bbabafa47
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-06-09 13:08:20 +0200

    Fix args in "func.arrayGet()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -4369,10 +4369,14 @@ final class LangPredefinedFunctions {
 			return null;
 		});
 		funcs.put("arrayGet", (argumentList, SCOPE_ID) -> {
-			DataObject arrPointerObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, true);
-			DataObject indexObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, false);
-			if(argumentList.size() > 0) //Not 2 arguments
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 2)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 2), SCOPE_ID);
+			if(combinedArgumentList.size() > 2)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 2), SCOPE_ID);
+			
+			DataObject arrPointerObject = combinedArgumentList.get(0);
+			DataObject indexObject = combinedArgumentList.get(1);
 			
 			if(arrPointerObject.getType() != DataType.ARRAY)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARR_PTR, SCOPE_ID);
