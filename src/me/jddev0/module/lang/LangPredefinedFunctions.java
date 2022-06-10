508e15f4250623240011b79e69858fc672725a97
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-06-10 12:56:59 +0200

    Fix args in "func.toChar()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -1820,9 +1820,13 @@ final class LangPredefinedFunctions {
 			return new DataObject().setInt(charObject.getChar());
 		});
 		funcs.put("toChar", (argumentList, SCOPE_ID) -> {
-			DataObject asciiValueObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, false);
-			if(argumentList.size() > 0) //Not 1 argument
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 1)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			if(combinedArgumentList.size() > 1)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			
+			DataObject asciiValueObject = combinedArgumentList.get(0);
 			
 			Number asciiValue = asciiValueObject.toNumber();
 			if(asciiValue == null)
