acc61faab1bd66fc67afeeb2a982913df18699f2
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-02 23:02:06 +0200

    Fix args in "func.muli()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -2405,10 +2405,11 @@ final class LangPredefinedFunctions {
 			}, SCOPE_ID);
 		});
 		funcs.put("muli", (argumentList, SCOPE_ID) -> {
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			
 			int prod = 1;
 			
-			while(argumentList.size() > 0) {
-				DataObject numberObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, true);
+			for(DataObject numberObject:combinedArgumentList) {
 				Number number = numberObject.toNumber();
 				if(number == null)
 					return interpreter.setErrnoErrorObject(InterpretingError.NO_NUM, SCOPE_ID);
