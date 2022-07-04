c9142bf75427b942aa799fc0eb8af4c4bdb83832
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-04 15:24:37 +0200

    Fix args in "func.ttoi()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -1743,9 +1743,13 @@ final class LangPredefinedFunctions {
 			return dataObject;
 		});
 		funcs.put("ttoi", (argumentList, SCOPE_ID) -> {
-			DataObject textObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, false);
-			if(argumentList.size() > 0) //Not 1 argument
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 1)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			if(combinedArgumentList.size() > 1)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			
+			DataObject textObject = combinedArgumentList.get(0);
 			
 			String str = textObject.getText();
 			try {
