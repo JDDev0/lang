0f8f2dd0ac99c3ca79196de48f532b84a49c2eea
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-08 23:06:46 +0200

    Fix args in "func.asFinal()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -1149,10 +1149,13 @@ final class LangPredefinedFunctions {
 			return null;
 		});
 		funcs.put("asFinal", (argumentList, SCOPE_ID) -> {
-			DataObject dataObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, false);
-			if(argumentList.size() > 0) //Not 1 argument
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 1)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			if(combinedArgumentList.size() > 1)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
 			
+			DataObject dataObject = combinedArgumentList.get(0);
 			return new DataObject(dataObject).setCopyStaticAndFinalModifiers(true).setFinalData(true);
 		});
 		funcs.put("isFinal", (argumentList, SCOPE_ID) -> {
