cd645d0668bf9bce2c9686694e86e49c0b4af928
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-09 07:48:01 +0200

    Fix args in "func.asStatic()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -1188,10 +1188,13 @@ final class LangPredefinedFunctions {
 			return null;
 		});
 		funcs.put("asStatic", (argumentList, SCOPE_ID) -> {
-			DataObject dataObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, false);
-			if(argumentList.size() > 0) //Not 1 argument
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 1)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			if(combinedArgumentList.size() > 1)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
 			
+			DataObject dataObject = combinedArgumentList.get(0);
 			return new DataObject(dataObject).setCopyStaticAndFinalModifiers(true).setStaticData(true);
 		});
 		funcs.put("isStatic", (argumentList, SCOPE_ID) -> {
