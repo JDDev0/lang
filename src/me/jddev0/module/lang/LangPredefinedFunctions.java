70eaca74d70f675dc027f1b91faac1647f357cbc
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-09 07:43:04 +0200

    Fix args in "func.isFinal()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -1159,10 +1159,13 @@ final class LangPredefinedFunctions {
 			return new DataObject(dataObject).setCopyStaticAndFinalModifiers(true).setFinalData(true);
 		});
 		funcs.put("isFinal", (argumentList, SCOPE_ID) -> {
-			DataObject dataObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, false);
-			if(argumentList.size() > 0) //Not 1 argument
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 1)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			if(combinedArgumentList.size() > 1)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
 			
+			DataObject dataObject = combinedArgumentList.get(0);
 			return new DataObject().setBoolean(dataObject.isFinalData());
 		});
 		funcs.put("makeStatic", (argumentList, SCOPE_ID) -> {
