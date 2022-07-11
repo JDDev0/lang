ffe01a606acd6f33d4d45310ae22d533dc1fbf0a
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-11 07:00:46 +0200

    Fix args in "func.typeOf()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -1379,9 +1379,13 @@ final class LangPredefinedFunctions {
 			return new DataObject().setBoolean(dataObject.getType() == dataTypeObject.getTypeValue());
 		});
 		funcs.put("typeOf", (argumentList, SCOPE_ID) -> {
-			DataObject dataObject = LangUtils.getNextArgumentAndRemoveUsedDataObjects(argumentList, true);
-			if(argumentList.size() > 0) //Not 1 argument
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 1)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			if(combinedArgumentList.size() > 1)
 				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			
+			DataObject dataObject = combinedArgumentList.get(0);
 			
 			return new DataObject().setTypeValue(dataObject.getType());
 		});
