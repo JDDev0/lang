bd6e1cb0ab9c59f9b48e913bb45313486c2f1d8e
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-16 19:41:26 +0200

    Fix args in "func.currentUnixTime()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -1072,7 +1072,12 @@ final class LangPredefinedFunctions {
 			
 			return new DataObject().setLong(System.currentTimeMillis());
 		});
-		funcs.put("currentUnixTime", (argumentList, SCOPE_ID) -> new DataObject().setLong(Instant.now().getEpochSecond()));
+		funcs.put("currentUnixTime", (argumentList, SCOPE_ID) -> {
+			if(argumentList.size() > 0)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			
+			return new DataObject().setLong(Instant.now().getEpochSecond());
+		});
 		funcs.put("repeat", (argumentList, SCOPE_ID) -> {
 			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
 			if(combinedArgumentList.size() < 2)
