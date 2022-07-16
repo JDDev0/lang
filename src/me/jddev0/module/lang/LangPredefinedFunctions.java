5e741849481df5a61523222c21a4cd4a50548442
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-16 16:42:31 +0200

    Fix args in "func.freeAllVars()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -928,6 +928,9 @@ final class LangPredefinedFunctions {
 			return null;
 		});
 		funcs.put("freeAllVars", (argumentList, SCOPE_ID) -> {
+			if(argumentList.size() > 0)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			
 			interpreter.resetVars(SCOPE_ID);
 			return null;
 		});
