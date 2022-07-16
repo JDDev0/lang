34a0a31cf32e5201030442fce5ba486627133148
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-16 19:22:49 +0200

    Fix args in "func.getErrorText()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -988,7 +988,12 @@ final class LangPredefinedFunctions {
 				return "func.getErrorText";
 			}
 		});
-		funcs.put("getErrorText", (argumentList, SCOPE_ID) -> new DataObject(interpreter.getAndClearErrnoErrorObject(SCOPE_ID).getErrorText()));
+		funcs.put("getErrorText", (argumentList, SCOPE_ID) -> {
+			if(argumentList.size() > 0)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			
+			return new DataObject(interpreter.getAndClearErrnoErrorObject(SCOPE_ID).getErrorText());
+		});
 		funcs.put("errorText", (argumentList, SCOPE_ID) -> {
 			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
 			if(combinedArgumentList.size() < 1)
