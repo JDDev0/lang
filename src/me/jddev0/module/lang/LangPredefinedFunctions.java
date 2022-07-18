fb426e8caed9a6676cc6aecc5edd8190469c61a4
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-07-18 07:20:50 +0200

    Add new function: "func.randf()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -2549,6 +2549,12 @@ final class LangPredefinedFunctions {
 			
 			return new DataObject().setLong(LangInterpreter.RAN.nextLong());
 		});
+		funcs.put("randf", (argumentList, SCOPE_ID) -> {
+			if(argumentList.size() > 0)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			
+			return new DataObject().setFloat(LangInterpreter.RAN.nextFloat());
+		});
 		funcs.put("inci", (argumentList, SCOPE_ID) -> {
 			return unaryMathOperationHelper(argumentList, number -> {
 				return new DataObject().setInt(number.intValue() + 1);
