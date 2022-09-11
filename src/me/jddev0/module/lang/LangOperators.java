4505aff4cefc0a8b521038f650ed7b46fae3273a
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-09-11 23:00:38 +0200

    Define the "|" operator for "ANY" and "FUNCTION_POINTER" as "Pipe" operator

--- src/me/jddev0/module/lang/LangOperators.java
+++ src/me/jddev0/module/lang/LangOperators.java
@@ -1723,6 +1723,14 @@ final class LangOperators {
 	 * For "|"
 	 */
 	public DataObject opOr(DataObject leftSideOperand, DataObject rightSideOperand, final int SCOPE_ID) {
+		if(rightSideOperand.getType() == DataType.FUNCTION_POINTER) {
+			FunctionPointerObject func = rightSideOperand.getFunctionPointer();
+			List<DataObject> args = new LinkedList<>();
+			args.add(leftSideOperand);
+			
+			return interpreter.callFunctionPointer(func, rightSideOperand.getVariableName(), args, SCOPE_ID);
+		}
+		
 		switch(leftSideOperand.getType()) {
 			case INT:
 				switch(rightSideOperand.getType()) {
