c32997e49a157ffad49a05eb94bbdde3fbd56bba
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-11-22 22:49:06 +0100

    Add the "exportFunction()" method in the "LangNativeModule" class

--- src/me/jddev0/module/lang/LangNativeModule.java
+++ src/me/jddev0/module/lang/LangNativeModule.java
@@ -21,6 +21,23 @@ public abstract class LangNativeModule {
 		this.module = module;
 	}
 	
+	protected final void exportFunction(String functionName, LangPredefinedFunctionObject func) {
+		for(int i = 0;i < functionName.length();i++) {
+			char c = functionName.charAt(i);
+			if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '_')
+				continue;
+			
+			throw new RuntimeException("The function name may only contain alphanumeric characters and underscore (_)");
+		}
+		
+		module.getExportedFunctions().add(functionName);
+		
+		//Create function object container to force function to be a normal function
+		interpreter.funcs.put(functionName, (argumentList, SCOPE_ID) -> {
+			return func.callFunc(argumentList, SCOPE_ID);
+		});
+	}
+	
 	/**
 	 * Will be called if the module is loaded
 	 * 
