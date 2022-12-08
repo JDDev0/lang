572901e481ec756e33f442acf9b2d689c4b70eb5
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-12-08 22:18:43 +0100

    Add new list function: "func.listCountOf()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -5835,6 +5835,23 @@ final class LangPredefinedFunctions {
 			
 			return null;
 		});
+		funcs.put("listCountOf", (argumentList, SCOPE_ID) -> {
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			if(combinedArgumentList.size() < 2)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 2), SCOPE_ID);
+			if(combinedArgumentList.size() > 2)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 2), SCOPE_ID);
+			
+			DataObject listObject = combinedArgumentList.get(0);
+			DataObject elementObject = combinedArgumentList.get(1);
+			
+			if(listObject.getType() != DataType.LIST)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARGUMENTS, String.format(ARGUMENT_TYPE_FORMAT, " 1", DataType.LIST), SCOPE_ID);
+			
+			List<DataObject> list = listObject.getList();
+			long count = list.stream().filter(ele -> ele.isStrictEquals(elementObject)).count();
+			return new DataObject().setLong(count);
+		});
 		funcs.put("listLength", (argumentList, SCOPE_ID) -> {
 			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
 			if(combinedArgumentList.size() < 1)
