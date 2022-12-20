aedaa78471027df61dab76958286394e6a8173c9
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-12-20 21:32:22 +0100

    Use "requireArgumentCount()" in "func.clearAllVars()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -423,10 +423,9 @@ final class LangPredefinedFunctions {
 			@Override
 			public DataObject callFunc(List<DataObject> argumentList, final int SCOPE_ID) {
 				List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
-				if(combinedArgumentList.size() < 1)
-					return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
-				if(combinedArgumentList.size() > 1)
-					return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+				DataObject error;
+				if((error = requireArgumentCount(combinedArgumentList, 1, SCOPE_ID)) != null)
+					return error;
 				
 				DataObject pointerObject = combinedArgumentList.get(0);
 				DataObject dereferencedVarPointer = null;
