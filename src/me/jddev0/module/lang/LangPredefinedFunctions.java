1417755e78e4383c03ebc072183a7a6f1b926d35
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-12-24 18:09:08 +0100

    Use "requireArgumentCount()" in "func.getErrorText()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -626,8 +626,10 @@ final class LangPredefinedFunctions {
 			}
 		});
 		funcs.put("getErrorText", (argumentList, SCOPE_ID) -> {
-			if(argumentList.size() > 0)
-				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			DataObject error;
+			if((error = requireArgumentCount(combinedArgumentList, 0, SCOPE_ID)) != null)
+				return error;
 			
 			return new DataObject(interpreter.getAndClearErrnoErrorObject(SCOPE_ID).getErrorText());
 		});
