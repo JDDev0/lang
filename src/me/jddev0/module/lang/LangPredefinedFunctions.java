2e3e06a4d51ed0fa2819eaab6798b5460748aa47
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-12-27 23:25:02 +0100

    Use "requireArgumentCount()" in "func.sleep()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -706,10 +706,9 @@ final class LangPredefinedFunctions {
 	private void addPredefinedSystemFunctions(Map<String, LangPredefinedFunctionObject> funcs) {
 		funcs.put("sleep", (argumentList, SCOPE_ID) -> {
 			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
-			if(combinedArgumentList.size() < 1)
-				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(NOT_ENOUGH_ARGUMENTS_FORMAT, 1), SCOPE_ID);
-			if(combinedArgumentList.size() > 1)
-				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 1), SCOPE_ID);
+			DataObject error;
+			if((error = requireArgumentCount(combinedArgumentList, 1, SCOPE_ID)) != null)
+				return error;
 			
 			DataObject dataObject = combinedArgumentList.get(0);
 			Number number = dataObject.toNumber();
