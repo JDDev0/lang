45707cbd42b836902d4b693ecf15028e3de75b13
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-12-28 08:26:17 +0100

    Use "requireArgumentCount()" in "func.nanoTime()", "func.currentTimeMillis()", and "func.currentUnixTime()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -724,20 +724,26 @@ final class LangPredefinedFunctions {
 			return null;
 		});
 		funcs.put("nanoTime", (argumentList, SCOPE_ID) -> {
-			if(argumentList.size() > 0)
-				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			DataObject error;
+			if((error = requireArgumentCount(combinedArgumentList, 0, SCOPE_ID)) != null)
+				return error;
 			
 			return new DataObject().setLong(System.nanoTime());
 		});
 		funcs.put("currentTimeMillis", (argumentList, SCOPE_ID) -> {
-			if(argumentList.size() > 0)
-				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			DataObject error;
+			if((error = requireArgumentCount(combinedArgumentList, 0, SCOPE_ID)) != null)
+				return error;
 			
 			return new DataObject().setLong(System.currentTimeMillis());
 		});
 		funcs.put("currentUnixTime", (argumentList, SCOPE_ID) -> {
-			if(argumentList.size() > 0)
-				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_ARG_COUNT, String.format(TOO_MANY_ARGUMENTS_FORMAT, 0), SCOPE_ID);
+			List<DataObject> combinedArgumentList = LangUtils.combineArgumentsWithoutArgumentSeparators(argumentList);
+			DataObject error;
+			if((error = requireArgumentCount(combinedArgumentList, 0, SCOPE_ID)) != null)
+				return error;
 			
 			return new DataObject().setLong(Instant.now().getEpochSecond());
 		});
