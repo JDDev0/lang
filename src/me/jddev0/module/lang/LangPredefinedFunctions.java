c43a3729af1fd3ba03a6e2b9a70273491088fcfb
JDDev0 <jddev0@dorneger.at>
2023-02-08 08:00:19 +0100

    Add new combinator functions: "func.combNW()" and "func.combNX()"

--- src/me/jddev0/module/lang/LangPredefinedFunctions.java
+++ src/me/jddev0/module/lang/LangPredefinedFunctions.java
@@ -3679,6 +3679,40 @@ final class LangPredefinedFunctions {
 			
 			return interpreter.callFunctionPointer(ret.getFunctionPointer(), ret.getVariableName(), argsACall2, SCOPE_ID);
 		}));
+		funcs.put("combNW", combinatorFunctionExternalFunctionObjectHelper(2, new int[] {0}, (Combinator2ArgFunction)(a, b, SCOPE_ID) -> {
+			FunctionPointerObject aFunc = a.getFunctionPointer();
+			
+			List<DataObject> argsACall1 = new LinkedList<>();
+			argsACall1.add(b);
+			
+			DataObject ret = interpreter.callFunctionPointer(aFunc, a.getVariableName(), argsACall1, SCOPE_ID);
+			ret = ret == null?new DataObject().setVoid():ret;
+			
+			if(ret.getType() != DataType.FUNCTION_POINTER)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_FUNC_PTR, "The value returned by a(b) must be of type " + DataType.FUNCTION_POINTER, SCOPE_ID);
+			
+			List<DataObject> argsACall2 = new LinkedList<>();
+			argsACall2.add(b);
+			
+			return interpreter.callFunctionPointer(ret.getFunctionPointer(), ret.getVariableName(), argsACall2, SCOPE_ID);
+		}));
+		funcs.put("combNX", combinatorFunctionExternalFunctionObjectHelper(3, new int[] {0}, (Combinator3ArgFunction)(a, b, c, SCOPE_ID) -> {
+			FunctionPointerObject aFunc = a.getFunctionPointer();
+			
+			List<DataObject> argsACall1 = new LinkedList<>();
+			argsACall1.add(c);
+			
+			DataObject ret = interpreter.callFunctionPointer(aFunc, a.getVariableName(), argsACall1, SCOPE_ID);
+			ret = ret == null?new DataObject().setVoid():ret;
+			
+			if(ret.getType() != DataType.FUNCTION_POINTER)
+				return interpreter.setErrnoErrorObject(InterpretingError.INVALID_FUNC_PTR, "The value returned by a(b) must be of type " + DataType.FUNCTION_POINTER, SCOPE_ID);
+			
+			List<DataObject> argsACall2 = new LinkedList<>();
+			argsACall2.add(b);
+			
+			return interpreter.callFunctionPointer(ret.getFunctionPointer(), ret.getVariableName(), argsACall2, SCOPE_ID);
+		}));
 		funcs.put("combM", combinatorFunctionExternalFunctionObjectHelper(1, new int[] {0}, (Combinator1ArgFunction)(a, SCOPE_ID) -> {
 			FunctionPointerObject aFunc = a.getFunctionPointer();
 			
